5dcee07e7cf8f5421cec415db87f3e81222c63dc
an0mal1a <129337574+an0mal1a@users.noreply.github.com>
2026-07-29 20:48:15 +0200

    feat(compiler): implement compiler builder

--- crates/lxdb-compiler/src/builder.rs
+++ crates/lxdb-compiler/src/builder.rs
@@ -1,3 +1,38 @@
 /// Builder used to configure the compiler.
 #[derive(Debug, Default)]
-pub struct Builder;
+pub struct Builder {
+    language: Option<String>,
+    dataset_name: Option<String>,
+    input: Option<String>,
+    output: Option<String>,
+}
+
+impl Builder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn language(mut self, language: impl Into<String>) -> Self {
+        self.language = Some(language.into());
+        self
+    }
+
+    pub fn dataset_name(mut self, name: impl Into<String>) -> Self {
+        self.dataset_name = Some(name.into());
+        self
+    }
+
+    pub fn input(mut self, input: impl Into<String>) -> Self {
+        self.input = Some(input.into());
+        self
+    }
+
+    pub fn output(mut self, output: impl Into<String>) -> Self {
+        self.output = Some(output.into());
+        self
+    }
+
+    pub fn build(self) -> Self {
+        self
+    }
+}
